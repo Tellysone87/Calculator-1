25a5227e2d48190fe0390c18177f4835618de7f6
Shantel <walterdtrt@aol.com>
2023-02-28 23:53:04 -0500

    Subtract function completed

--- arithmetic.py
+++ arithmetic.py
@@ -4,13 +4,14 @@
 def add(num1, num2):
     """Return the sum of num1 and num2."""
 
-
     return num1 + num2
 
 
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
+    return num1 - num2
 
+    
 
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
