9cc96c5ed4ef9ff157921d04946bc2b0ac6d4874
Shantel <walterdtrt@aol.com>
2023-03-01 00:01:33 -0500

    completed functions

--- arithmetic.py
+++ arithmetic.py
@@ -15,23 +15,29 @@ def subtract(num1, num2):
 
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
+    return num1 * num2
 
 
 def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
+    return num1 / num2
 
 
 def square(num1):
     """Return the square of num1."""
+    return pow(num1,2)
 
 
 def cube(num1):
     """Return the cube of num1."""
+    return pow(num1,3)
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
+    return pow(num1,num2)
 
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    return num1 % num2
